d57cb40fba5806af18a2cc05300d1d815660ac70
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-08-10 20:09:14 +0300

    Add comments.

--- wordament-august-2013/src/wordament.clj
+++ wordament-august-2013/src/wordament.clj
@@ -4,26 +4,32 @@
             [clojure.set :refer (union)])
   (:gen-class))
 
-(defn add-word [tree [fst & rst]]
+(defn add-word
+  "Добавляет слово в дерево Бора и возвращает новое дерево."
+  [tree [fst & rst]]
   (if (nil? fst)
     (assoc tree :word? true)
     (let [subtree (tree fst {})]
       (assoc tree fst (add-word subtree rst)))))
 
-(defn load-dictionary []
+(defn load-dictionary
+  "Загружает словарь из zip файла, а так же удаляет все слова c < 3 и > 16 буквами.
+  Возвращает список слов."
+  []
   (let [zip (-> (io/resource "dict.zip")
-                        (.openStream)
-                        (java.util.zip.ZipInputStream.))]
-            (.getNextEntry zip)
-            (->> zip
-                 io/reader
-                 line-seq
-                 (map lower-case)
-                 (filter #(< 2 (count %) 17))
-                 (filter #(neg? (.indexOf % "-"))))))
+                (.openStream)
+                (java.util.zip.ZipInputStream.))]
+    (.getNextEntry zip)
+    (->> zip
+         io/reader
+         line-seq
+         (map lower-case)
+         (filter #(< 2 (count %) 17))
+         (filter #(neg? (.indexOf % "-"))))))
 
 (def cost-string "А:1,Б:5,В:2,Г:4,Д:4,Е:1,Ё:0,Ж:6,З:4,И:1,Й:5,К:3,Л:2,М:2,Н:1,О:1,П:3,Р:2,С:2,Т:2,У:3,Ф:7,Х:5,Ц:7,Ч:5,Ш:4,Щ:0,Ъ:0,Ы:4,Ь:5,Э:10,Ю:4,Я:3")
 
+; Мапка со ценами букв.
 (def costs (->> (split cost-string #",")
                 (map lower-case)
                 (map (fn [[letter _ digit]]
@@ -33,7 +39,9 @@
 (defn word-price [word]
   (reduce + (map costs word)))
 
-(defn neibs [x y]
+(defn neibs
+  "Строит список всех возможных соседей клетки (x, y)."
+  [x y]
   (for [nx (range (dec x) (+ x 2))
         ny (range (dec y) (+ y 2))
         :when (and (<= 0 nx 3)
@@ -42,17 +50,28 @@
                              (= ny y))))]
     [nx ny]))
 
-(defn build-field [table]
+(defn build-field
+  "Строит вместо поля букв мапку, в которой ключ - координаты буквы, а значениe - мапка с самой буквой и соседями.
+  Таким образом можно быстро получить всех соседей конкретной клетки, без необходимости использовать neibs."
+  [table]
   (let [table (map lower-case table)]
-   (->> (for [i (range 4)
-              j (range 4)]
-          [[i j] {:letter (nth (nth table i) j)
-                  :neibs (neibs i j)}])
-        (into {}))))
+    (->> (for [i (range 4)
+               j (range 4)]
+           [[i j] {:letter (nth (nth table i) j)
+                   :neibs (neibs i j)}])
+         (into {}))))
 
+; В этой переменной будет находится поле.
 (def ^:dynamic *field*)
 
-(defn dfs [cur visited tree accum]
+(defn dfs
+  "Поиск слов через поиск в глубину.
+  cur - координаты текущей клетки
+  visited - множество посещённых координат
+  tree - текущее поддерево Бора в котором мы находимся
+  accum - текущие накопленные буквы
+  Возвращает множество найденных слов."
+  [cur visited tree accum]
   (let [res (if (:word? tree) #{accum} #{})
         possible-moves (->> (*field* cur)
                             :neibs
@@ -66,19 +85,21 @@
          (reduce union res)
          doall)))
 
-(defn find-words [table]
+(defn find-words
+  "Ищет и печатает слова из таблицы, отсортированные по стоимости."
+  [table]
   (binding [*field* (build-field table)]
-   (let [_ (println "Построение дерева Бора по словарю")
-         tree (time (reduce add-word {} (load-dictionary)))
-         _ (println "Поиск слов")
-         words (time (->> (for [[pos {:keys [letter]}] *field*
-                                :when (tree letter)]
-                            (dfs pos #{letter} (tree letter) (str letter)))
-                          (reduce union)
-                          (sort-by #(- (word-price %)))))]
-     (doseq [word words]
-       (println word (word-price word)))
-     (println "Всего:" (count words)))))
+    (let [_ (println "Построение дерева Бора по словарю")
+          tree (time (reduce add-word {} (load-dictionary)))
+          _ (println "Поиск слов")
+          words (time (->> (for [[pos {:keys [letter]}] *field*
+                                 :when (tree letter)]
+                             (dfs pos #{letter} (tree letter) (str letter)))
+                           (reduce union)
+                           (sort-by #(- (word-price %)))))]
+      (doseq [word words]
+        (println word (word-price word)))
+      (println "Всего:" (count words)))))
 
 (defn -main [& args]
   (if (empty? args)
